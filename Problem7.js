e9183d915c0bfbe25c62e48bc42c40151c25edd3
Md Jeshan Khan <jeshank1123@gmail.com>
2025-08-19 17:44:15 +0600

    first commit

---
+++ Problem7.js
@@ -0,0 +1,11 @@
+function findEvens(arr) {
+  let evens = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      evens.push(arr[i]);
+    }
+  }
+  return evens;
+}
+
+console.log(findEvens([1, 2, 3, 4, 5, 6]));
